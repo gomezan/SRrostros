b3fc8b87bccc388de3293ed475d549bbde103023
Andrés David Gómez Bautista <gomezan@javeriana.edu.co>
2022-08-05 11:12:31 -0500

    pruebaModelosDocumentada

--- pruebaModelos/pruebaModelos/ESPCN/miPropuesta/playGround.py
+++ pruebaModelos/pruebaModelos/ESPCN/miPropuesta/playGround.py
@@ -20,8 +20,9 @@ import torch.backends.cudnn as cudnn
 from pruebaModelos.ESPCN.models import ESPCN
 from pruebaModelos.ESPCN.utils import convert_ycbcr_to_rgb, preprocess, preprocessRGB
 
+#Este script busca calcular el fid de grupos de imágenes
 
-
+#Calcula el fid con base en los vectores de caracteristicas de ambos grupos de imagenes
 def calculate_fid(act1, act2):
 
     mu1, sigma1 = act1.mean(axis=0), cov(act1, rowvar=False)
@@ -37,13 +38,8 @@ def calculate_fid(act1, act2):
     fid = ssdiff + trace(sigma1 + sigma2 - 2.0 * covmean)
     return fid
 
-def uploadImage(path):
-    imagenes=[]
-    files=glob.glob(path)
-    for i in files:
-        imagenes.append(cv2.imread(i, 1))
-    return np.array(imagenes)
 
+#Calcula la descomposición de caracteristicas de un grupo de imagenes y las almacena como npy
 def calularActivacion(model, path, nombre):
     act = []
     files = glob.glob(path)
@@ -54,6 +50,7 @@ def calularActivacion(model, path, nombre):
     res=np.array(act)
     np.save(r"C:\Users\Estudiante\Documents\dataset\resultados\FID\activacion"+"\IMG"+nombre,res)
 
+#Calcula la activación de imágenes interpoladas dado método de interpolación
 def calularActivacionMetodo(model, path, nombre, tecnica, factor):
     act = []
     files = glob.glob(path)
@@ -69,7 +66,9 @@ def calularActivacionMetodo(model, path, nombre, tecnica, factor):
     np.save(r"C:\Users\Estudiante\Documents\dataset\resultados\FID\activacion"+"\IMG"+nombre,res)
 
 
+#Calcula la activación de imágenes en super resolución empleando la ESPCN en RGB
 def calularActivacionModeloRGB(model, path, nombre, factor, weights_file):
+    #lista para almacenar los resultados
     act = []
     files = glob.glob(path)
 
@@ -120,7 +119,9 @@ def calularActivacionModeloRGB(model, path, nombre, factor, weights_file):
     np.save(r"C:\Users\Estudiante\Documents\dataset\resultados\FID\activacion"+"\IMG"+nombre,res)
 
 
+#Calcula la activación de imágenes en super resolución empleando la ESPCN en Y
 def calularActivacionModeloY(model, path, nombre, factor, weights_file):
+    # lista para almacenar los resultados
     act = []
     files = glob.glob(path)
 
@@ -165,7 +166,7 @@ def calularActivacionModeloY(model, path, nombre, factor, weights_file):
     res=np.array(act)
     np.save(r"C:\Users\Estudiante\Documents\dataset\resultados\FID\activacion"+"\IMG"+nombre,res)
 
-
+#Permite mapear tecnica con el nombre de dicha tecnica
 nombres={
     "lan": "lanczos",
     "nn": "NEAREST",
@@ -175,6 +176,7 @@ nombres={
     "y": "Y"
 }
 
+#Permite mapear tecnica con el string de opencv que describe dicha técnica
 interpolacion={
     "lan": cv2.INTER_LANCZOS4,
     "nn": cv2.INTER_NEAREST,
@@ -182,26 +184,35 @@ interpolacion={
     "bi3": cv2.INTER_CUBIC,
 }
 
+#Permite mapear tecnica con el método de calculo de activación de dicha técnica
 proImagen={
     "rgb": calularActivacionModeloRGB,
     "y": calularActivacionModeloY
 }
 
+#Permite mapear la escala con laubicación de los pesos de dicha escala/factor
 pesos={
     2: r"C:\Users\Estudiante\Documents\dataset\prueba\espcn_x2Full.pth",
     4: r"C:\Users\Estudiante\Documents\dataset\prueba\best0x4.pth",
     8: r"C:\Users\Estudiante\Documents\dataset\prueba\espcnx8.pth"
 }
 
+#Clase encargada de calcular FID
 class CalculadorFid:
 
+    #Calula fid
     def calcularFid(self, red,mtd,escala):
 
+        #Obtengo nombre del método de interpolación
         interpolationName=nombres[mtd]
+        #Si es una tecnica de super resoluciuón
         if(red):
+            #Carga del método deacuerdo si es RGB y Y
             procedimiento=proImagen[mtd]
             weight = pesos[escala]
+        # Si es una tecnica de interpolación
         else:
+            #Carga del indicativo de opencv del método de interpolación
             tcn = interpolacion[mtd]
 
         print("Inicio")
@@ -210,23 +221,30 @@ class CalculadorFid:
         model = InceptionV3(include_top=False, pooling='avg', input_shape=(1024 ,1024 ,3))
         print("Modelo creado")
 
-        # Caluclo de vectores de caracteristicas
+        # Caluclo de vectores de caracteristicas del ground truth
+            #Si no existe ... debe ser calculado
         if not (os.path.exists(r'C:\Users\Estudiante\Documents\dataset\resultados\FID\activacion\IMGgt.npy')):
             calularActivacion(model, r"C:\Users\Estudiante\Documents\dataset\groundTruth\eval\*.png", "gt.npy")
 
+        #carga del vector de caracteristicas del ground truth
         act1=np.load(r'C:\Users\Estudiante\Documents\dataset\resultados\FID\activacion\IMGgt.npy')
         print("Ground truth cargado")
 
+        #Si es una técnica de interpolación
         if not(red):
 
+            # Si no existe ... debe ser calculado
             if not (os.path.exists(r'C:\Users\Estudiante\Documents\dataset\resultados\FID\activacion\IMG'+interpolationName+str(escala)+'.npy')):
                 print("no existe activacion " + interpolationName)
                 calularActivacionMetodo(model, r"C:\Users\Estudiante\Documents\dataset\decimadasX"+str(escala)+"\eval\*.png", interpolationName+str(escala)+".npy", tcn, escala)
 
+            # carga del vector de caracteristicas del método
             act2=np.load(r'C:\Users\Estudiante\Documents\dataset\resultados\FID\activacion\IMG'+interpolationName+str(escala)+'.npy')
             print("metodo cargado")
 
+        # Si es una técnica de super resolución
         else:
+            # Si no existe ... debe ser calculado
             if not (os.path.exists(
                     r'C:\Users\Estudiante\Documents\dataset\resultados\FID\activacion\IMG' + interpolationName + str(
                             escala) + '.npy')):
@@ -234,12 +252,13 @@ class CalculadorFid:
                 procedimiento(model, r"C:\Users\Estudiante\Documents\dataset\decimadasX"+str(escala)+"\eval\*.png",
                                         interpolationName + str(escala) + ".npy", escala, weight)
 
+            # carga del vector de caracteristicas del método
             act2 = np.load(r'C:\Users\Estudiante\Documents\dataset\resultados\FID\activacion\IMG' + interpolationName + str(
                 escala) + '.npy')
             print("metodo cargado")
 
 
-
+        #calculo del FID
         print("Iniciando calculo")
         fid = calculate_fid( act1, act2)
         print('FID (different): %.3f' % fid)
